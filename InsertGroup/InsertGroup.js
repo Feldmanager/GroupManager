ccf1b1ae96f99cec504e25f1b90e067764c7b1e9
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-10-05 17:36:46 +0300

    yes

--- InsertGroup/InsertGroup.js
+++ InsertGroup/InsertGroup.js
@@ -4,11 +4,43 @@ var router = express.Router()
 
 
 app.listen(3000, ()=>{
-    console.log("listennig")
+    console.log("listenniggggg")
 });
 
-app.get('/getter', (req,res)=>{
-res.send("fuck");
-})
+app.post('/Insert', (req,res)=>{
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'feldmanager',
+        password: 'littlecitizen1!',
+        server: '10.1.0.117', 
+        database: 'feldmanager' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('insert into Groups (TypeId,GroupName) values (${req.body.TypeId}, ${req.body.GroupName})', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+
+app.post('/check', (req,res)=>{
+    console.log("success");
+});
 
 module.exports = router;
