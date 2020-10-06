80568e4873cc3b7b4c3699ee941f989f07fcdf69
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-10-06 15:02:07 +0300

    add fixes

---
+++ Group/BL/GroupHandler.js
@@ -0,0 +1,14 @@
+const val = require('../../Common/Validation')
+const SqlHandler = require('../../DAL/SqlHandler');
+
+const InsertGroup=async (elements)=>
+{
+    var result;
+    val(elements);
+    var query = `EXEC InsertGroup @TypeId=${parseInt(elements.TypeId)}, @GroupName='${(elements.GroupName).toString()}'`;
+    var sqlExecute = new SqlHandler();
+    result = await sqlExecute.Execute(query);
+    return result;
+}
+
+module.exports=InsertGroup;
