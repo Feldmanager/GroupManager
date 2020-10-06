59eb923276a22425b358e921997a99d1072dec83
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-10-06 15:02:58 +0300

    fix in DAL

---
+++ DAL/SqlHandler.js
@@ -0,0 +1,26 @@
+var sql = require("mssql");
+
+class SqlHandler {
+    constructor() {
+        this.config = {
+            user: 'feldmanager',
+            password: 'littlecitizen1!',
+            server: '10.1.0.117',
+            database: 'feldmanager'
+        };
+    }
+
+    async Execute(query) {
+        let result;
+        let pool = await sql.connect(this.config)
+        let request = pool.request();
+        await request.query(query)
+            .then((recordset) => {
+                result = recordset;
+
+            });
+        return result;
+    }
+}
+
+module.exports = SqlHandler
